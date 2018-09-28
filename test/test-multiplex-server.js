e828ce9f2f8b6984d60c31bfb50705ee6fc91930
Evan Sangaline <esangaline@gmail.com>
2018-09-28 14:39:06 -0400

    Add tests for reusing sockets to forward connections.

--- test/test-multiplex-server.js
+++ test/test-multiplex-server.js
@@ -18,8 +18,10 @@ describe('MultiplexServer', () => {
   let httpServer;
   let httpsServer;
 
-  let multiplexPort;
+  let multiplexPortForwardingPort;
   let multiplexPortForwardingServer;
+  let multiplexDirectConnectionPort;
+  let multiplexDirectConnectionServer;
 
   before(async () => {
     // Create an axios instance that ignores certificate errors.
@@ -55,14 +57,24 @@ describe('MultiplexServer', () => {
       httpsServer.listen(httpsPort, error => error ? reject(error) : resolve());
     });
 
-    // Start an multiplex server.
-    multiplexPort = await getPort();
+    // Start a multiplex server to forward request to ports.
+    multiplexPortForwardingPort = await getPort();
     multiplexPortForwardingServer = new MultiplexServer({
       http: httpPort,
       https: httpsPort,
     });
     await new Promise((resolve, reject) => {
-      multiplexPortForwardingServer.listen(multiplexPort, error => error ? reject(error) : resolve());
+      multiplexPortForwardingServer.listen(multiplexPortForwardingPort, error => error ? reject(error) : resolve());
+    });
+
+    // Start a multiplex server to directly connect sockets to the servers.
+    multiplexDirectConnectionPort = await getPort();
+    multiplexDirectConnectionServer = new MultiplexServer({
+      http: httpServer,
+      https: httpsServer,
+    });
+    await new Promise((resolve, reject) => {
+      multiplexDirectConnectionServer.listen(multiplexDirectConnectionPort, error => error ? reject(error) : resolve());
     });
   });
 
@@ -81,13 +93,25 @@ describe('MultiplexServer', () => {
   });
 
   it('forward an HTTP request to the HTTP port', async () => {
-    const response = await axios.get(`http://localhost:${multiplexPort}/`);
+    const response = await axios.get(`http://localhost:${multiplexPortForwardingPort}/`);
     assert(response.status === 200);
     assert(response.data === 'httpServer')
   });
 
   it('forward an HTTPS request to the HTTPS port', async () => {
-    const response = await axios.get(`https://localhost:${multiplexPort}/`);
+    const response = await axios.get(`https://localhost:${multiplexPortForwardingPort}/`);
+    assert(response.status === 200);
+    assert(response.data === 'httpsServer')
+  });
+
+  it('connect an HTTP request socket to the HTTP server', async () => {
+    const response = await axios.get(`http://localhost:${multiplexDirectConnectionPort}/`);
+    assert(response.status === 200);
+    assert(response.data === 'httpServer')
+  });
+
+  it('connect an HTTP request socket to the HTTPS server', async () => {
+    const response = await axios.get(`https://localhost:${multiplexDirectConnectionPort}/`);
     assert(response.status === 200);
     assert(response.data === 'httpsServer')
   });
